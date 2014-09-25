6e85782ea4ef37022b0ea0d828f47d95c06afee9
s.vandamme <sebastien.vandamme@gmail.com>
2014-09-25 21:54:18 +0200

    refactoring cont'd

--- src/main/java/com/telemis/exercice/score/calculator/calculators/SpareScoreCalculator.java
+++ src/main/java/com/telemis/exercice/score/calculator/calculators/SpareScoreCalculator.java
@@ -1,6 +1,7 @@
 package com.telemis.exercice.score.calculator.calculators;
 
 import com.telemis.exercice.game.Lancer;
+import com.telemis.exercice.game.frame.FourLaunchFrame;
 import com.telemis.exercice.game.frame.Frame;
 import com.telemis.exercice.score.ScoreContainer;
 import com.telemis.exercice.score.calculator.ScoreCalculatorFactory;
@@ -11,10 +12,9 @@ import java.util.List;
 
 /**
  * Created by sebastien.vandamme@gmail.com on 20/09/2014.
- *
+ * <p/>
  * Classe permettant de calculer le score d'une frame lors d'un spare.
  */
-//TODO quid si spare dans dernière frame ?
 public class SpareScoreCalculator implements ScoreCalculator {
     private static final String SPARE_SYMBOL = "/";
 
@@ -35,26 +35,43 @@ public class SpareScoreCalculator implements ScoreCalculator {
         ScoreCalculator scoreCalculator = ScoreCalculatorFactory.createScoreCalculator(ScoreCalculatorType.NORMAL);
         ScoreContainer container = scoreCalculator.calculate(frames, frame);
         int frameScore = container.getFrameScore();
+        boolean lastFrame = frame instanceof FourLaunchFrame;
 
-        completeRepresentation(container, frame.getLancers().size());
+        completeRepresentation(container, frame.getLancers().size(), lastFrame);
 
-        List<Lancer> nextFrameLancers = frames.get(frames.indexOf(frame) + 1).getLancers();
+        if (!lastFrame) {
+            List<Lancer> nextFrameLancers = frames.get(frames.indexOf(frame) + 1).getLancers();
 
-        for (int i = 0; i < 2; ++i) {
-            final int fallenPins = nextFrameLancers.get(i).getFallenPins();
-            frameScore += fallenPins;
+            for (int i = 0; i < 2; ++i) {
+                final int fallenPins = nextFrameLancers.get(i).getFallenPins();
+                frameScore += fallenPins;
+            }
+
+            container.setFrameScore(frameScore);
         }
-
-        container.setFrameScore(frameScore);
 
         return container;
     }
 
-    private void completeRepresentation(ScoreContainer container, int lancersSize) {
-        container.getLancersScores().set(lancersSize - 1, SPARE_SYMBOL);
+    private void completeRepresentation(ScoreContainer container, int launchesSize, boolean lastFrame) {
+        if (lastFrame) {
+            int currentScore = 0;
+            final List<String> lancersScores = container.getLancersScores();
 
-        if (container.getLancersScores().size() == 2) {
-            container.getLancersScores().add(StringUtils.EMPTY);
+            for (int i = 0; i < lancersScores.size(); ++i) {
+                currentScore += Integer.valueOf(lancersScores.get(i));
+
+                if (currentScore == 15) {
+                    container.getLancersScores().set(i, SPARE_SYMBOL);
+                    break;
+                }
+            }
+        } else {
+            container.getLancersScores().set(launchesSize - 1, SPARE_SYMBOL);
+
+            if (container.getLancersScores().size() == 2) {
+                container.getLancersScores().add(StringUtils.EMPTY);
+            }
         }
     }
 }
